b1f7d04e47f56ca9d1b2e254a97cfa5248a6112f
carmenne <=>
2018-01-06 23:15:26 +0100

    Kata 1 Counting Duplicates

---
+++ kata.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the input argument and store it, toghether with the length
+var=$(echo $1 | grep -oi . | sort -i | uniq -i)
+echo $var
+# Initialize the counter that will kep track of chars that
+# appear multiple times
+counter=0
+
+# Interate char by char through the string input argument and
+# store the char in an array
+
+for c in $var; do
+
+	number=$(echo $1 | grep -oi $c | wc -w)
+	if [ $number -gt 1 ]; then
+		counter=$(($counter+1))
+	fi
+done
+
+echo $counter
